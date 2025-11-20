55a5ab58c1345bbb9bf29c56ad4915a12f8f4ddc
KD54321 <98127805+KD54321@users.noreply.github.com>
2025-11-20 14:42:29 -0500

    endpoints

---
+++ assets/js/api.js
@@ -0,0 +1,35 @@
+'use strict';
+require('dotenv').config();
+const api_key = process.env.api_key;
+/**
+ * 
+ * @param {string} URL API url 
+ * @param {Function} callback callback
+ */
+export const fetchData = function(URL, callback){
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res=> res.json())
+    .then(data=>callback(data))
+}
+
+export const url = {
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}${lon}&units=metric`
+    },
+    airPollution(lat, lon){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}${lon}`
+    },
+    reverseGeo(lat, lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} query Search query e.g.: "Montreal", "New York"
+     */
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
