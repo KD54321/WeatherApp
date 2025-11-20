4e668dd4bd80c82205d35f2a772acc33e5da3cb2
KD54321 <98127805+KD54321@users.noreply.github.com>
2025-11-20 15:28:26 -0500

    js

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import {fetchDate, url} from "./api.js"
+import * as module from "./module.js"
+
+/**
+ * 
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event type e.g.: "click", "mouseover" 
+ * @param {Function} callback Callback function 
+ */
+const addEventOnElements = function(elements, eventType, callback){
+    for(const element of elements) element.addEventListener(eventType, callback);
+}
+
+/**
+ * Toggle search in mobile devices
+ */
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
